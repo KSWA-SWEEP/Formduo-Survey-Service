9e0b7294cb9eab6bcbe7834c7813f1d5ffcea5c9
banggeunho <panggeunho@gmail.com>
2022-12-03 17:26:15 +0900

    timezone 설정 추가

--- src/main/java/com/sweep/surveyservice/SurveyServiceApplication.java
+++ src/main/java/com/sweep/surveyservice/SurveyServiceApplication.java
@@ -6,11 +6,21 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.TimeZone;
+
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
 @EnableMongoRepositories(basePackages = "com.sweep.surveyservice.domain")
 public class SurveyServiceApplication {
 
+    @PostConstruct
+    public void started() {
+        System.out.println("현재시각 : " + new Date());
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        System.out.println("현재시각 : " + new Date());
+    }
     public static void main(String[] args) {
         SpringApplication.run(SurveyServiceApplication.class, args);
     }
